879c70686a8b18df43360982b84b852a0450f653
HoonInPark <hooninpark@gmail.com>
2023-09-15 21:11:47 +0900

    file management

---
+++ Source/UE_CombatPlane_lec/public/CP_AI_Pawn.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "CP_AI_Pawn.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE_COMBATPLANE_LEC_API UCP_AI_Pawn : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
